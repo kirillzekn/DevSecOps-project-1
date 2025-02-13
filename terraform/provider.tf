2aad35d1afb1e656d30c3f2e0291d5f52d23d994
Kirill <Kirill@Kirill.com>
2025-02-13 13:30:20 +0500

    providre change

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -6,11 +6,14 @@ provider "azuread" {
   #subscription_id = var.TF_VAR_AZURE_SUBSCRIPTION_ID
 }
 
+provider "azurerm" {
+}
+
 terraform {
     required_providers {
         azurerm = {
-        source  = "hashicorp/azurerm"
-        version = "=4.1.0"
+        source  = "hashicorp/azuread"
+        version = "2.0.0"
         }
     }
 }
