abb0e5cdd3cc7305ab0380903535b7ba7a67ae4b
Kirill <Kirill@Kirill.com>
2025-02-13 13:09:47 +0500

    backend config added

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,10 +1,5 @@
 terraform {
     backend "azurerm" {
-        resource_group_name   = "rg-terraform-state"
-        storage_account_name  = "tfstate02132025"
-        container_name        = "tfstate"
-        key                   = "terraform.tfstate"
-        tenant_id = var.TF_VAR_AZURE_TENANT_ID
-        subscription_id = var.TF_VAR_AZURE_SUBSCRIPTION_ID 
+
     }
 }
