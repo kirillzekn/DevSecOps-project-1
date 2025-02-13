721a14961df6ac35c863fd42b1ff7241ba442954
Kirill <Kirill@Kirill.com>
2025-02-13 13:26:57 +0500

    changed provider to azuread

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,9 +1,9 @@
-provider "azurerm" {
-  features {}
+provider "azuread" {
+  #features {}
   client_id       = var.TF_VAR_AZURE_CLIENT_ID
   client_secret   = var.TF_VAR_AZURE_CLIENT_SECRET
   tenant_id       = var.TF_VAR_AZURE_TENANT_ID
-  subscription_id = var.TF_VAR_AZURE_SUBSCRIPTION_ID
+  #subscription_id = var.TF_VAR_AZURE_SUBSCRIPTION_ID
 }
 
 terraform {
