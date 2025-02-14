fb1e7435ee0cd13875bc9b828b6d64310f023bbf
Kirill <Kirill@Kirill.com>
2025-02-14 15:35:53 +0500

    TF change

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,8 +1,5 @@
 provider "azuread" {
   #features {}
-  client_id     = var.TF_VAR_AZURE_CLIENT_ID
-  client_secret = var.TF_VAR_AZURE_CLIENT_SECRET
-  tenant_id     = var.TF_VAR_AZURE_TENANT_ID
   #subscription_id = var.TF_VAR_AZURE_SUBSCRIPTION_ID
 }
 
