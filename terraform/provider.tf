5314fefb39e3c0a530ff05f1ead2da2f479a0429
Kirill <Kirill@Kirill.com>
2025-02-14 14:53:06 +0500

    provider updated

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -10,10 +10,10 @@ provider "azurerm" {
 }
 
 terraform {
-    required_providers {
-        azurerm = {
-        source  = "hashicorp/azuread"
-        version = "2.0.0"
-        }
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=4.1.0"
     }
+  }
 }
