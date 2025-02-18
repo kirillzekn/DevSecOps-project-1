c28b3295be3ba9d1ad944e0257d840702d0d4005
Kirill <Kirill@Kirill.com>
2025-02-18 11:28:48 +0500

    depends_on

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,4 +10,5 @@ module "aks" {
   # insert the 1 required variable here
   resource_group_name = azurerm_resource_group.aks.name
   sku_tier            = "Free"
+  depends_on = [azurerm_resource_group.aks]
 }
