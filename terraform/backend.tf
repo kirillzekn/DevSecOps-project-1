46196e6fd8b0f1730569cff4181bbde151857ef0
Kirill <Kirill@Kirill.com>
2025-02-18 11:12:01 +0500

    commit

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,5 +1,9 @@
 terraform {
   backend "azurerm" {
+      resource_group_name   = "rg-terraform-state"
+      storage_account_name  = "tfstate02132025"
+      container_name        = "tfstate"
+      key                   = "terraform.tfstate"
 
   }
 }
