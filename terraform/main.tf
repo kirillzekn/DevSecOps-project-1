ae735f791dc066c0166e399aca62e6c8db3f7a39
Kirill <Kirill@Kirill.com>
2025-02-18 10:51:46 +0500

    aks

---
+++ terraform/main.tf
@@ -0,0 +1,13 @@
+resource "azurerm_resource_group" "aks" {
+  name     = "zekn_aks_rg"
+  location = "West Europe"
+
+}
+
+module "aks" {
+  source  = "Azure/aks/azurerm"
+  version = "9.4.1"
+  # insert the 1 required variable here
+  resource_group_name = azurerm_resource_group.aks.name
+  sku_tier            = "Free"
+}
