9b7032a50bdb0fa13b6448fdfc6275c2e92258a5
Kirill <Kirill@Kirill.com>
2025-03-05 13:59:47 +0500

    TF_VAR env variables

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "rg-terraform-state"
-    storage_account_name = "tfstate02132025"
-    container_name       = "tfstate"
+    resource_group_name  = var.TF_VAR_TERRAFORM_AZURE_RG_NAME
+    storage_account_name = var.TF_VAR_TERRAFORM_AZURE_STORAGE_ACCOUNT_NAME
+    container_name       = var.TF_VAR_TERRAFORM_AZURE_STORAGE_CONTAINER_NAME
     key                  = "terraform.tfstate"
 
   }
