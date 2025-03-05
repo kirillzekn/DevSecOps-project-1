6e57d3e7f978e9baba21a7fe68526c8523e8bd85
Kirill <Kirill@Kirill.com>
2025-03-05 14:45:54 +0500

    TF commit

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -39,3 +39,21 @@ variable "aks-identity_type" {
   default     = "SystemAssigned"
 
 }
+
+variable "TERRAFORM_AZURE_RG_NAME" {
+  type        = string
+  description = "The name of the resource group to deploy the AKS cluster"
+  
+}
+
+variable "TERRAFORM_AZURE_STORAGE_ACCOUNT_NAME" {
+  type        = string
+  description = "The name of the storage account to store the Terraform state file"
+  
+}
+
+variable "TERRAFORM_AZURE_STORAGE_CONTAINER_NAME" {
+  type        = string
+  description = "The name of the storage container to store the Terraform state file"
+  
+}
