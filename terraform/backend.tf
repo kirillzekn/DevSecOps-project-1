7e5b65376704ee9b3f6ad6c76895aca9dde4bc1e
Kirill <Kirill@Kirill.com>
2025-03-05 15:24:36 +0500

    changes

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -8,7 +8,6 @@ data "terraform_remote_state" "state" {
     resource_group_name  = var.TERRAFORM_AZURE_RG_NAME
     storage_account_name = var.TERRAFORM_AZURE_STORAGE_ACCOUNT_NAME
     container_name       = var.TERRAFORM_AZURE_STORAGE_CONTAINER_NAME
-    location             = var.AZURE_DEFAULT_REGION
     key                  = "terraform.tfstate"
   }
 }
